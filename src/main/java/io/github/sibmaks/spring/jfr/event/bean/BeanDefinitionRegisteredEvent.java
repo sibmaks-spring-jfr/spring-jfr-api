abe69e2b690ece14a27ef385b4385ff327a86846
sibmaks <sibmaks@vk.com>
2024-10-23 00:19:52 +0300

    Разделить инструментацию инициализации bean-ов на отдельные события #1 - extend Bean Definition Registered event

--- src/main/java/io/github/sibmaks/spring/jfr/event/bean/BeanDefinitionRegisteredEvent.java
+++ src/main/java/io/github/sibmaks/spring/jfr/event/bean/BeanDefinitionRegisteredEvent.java
@@ -17,14 +17,29 @@ import lombok.Getter;
 @Category("Spring Beans Flight Recorder")
 @Label("Bean Definition Registered")
 public class BeanDefinitionRegisteredEvent extends Event {
+    @Label("Bean scope")
+    private final String scope;
+    @Label("Bean class name")
+    private final String beanClassName;
     @Label("Bean name")
     private final String beanName;
+    @Label("Bean primary flag")
+    private final boolean primary;
     @Label("Bean's dependencies")
     private final String dependencies;
 
     @Builder
-    public BeanDefinitionRegisteredEvent(String beanName, String[] dependencies) {
+    public BeanDefinitionRegisteredEvent(
+            String scope,
+            String beanClassName,
+            String beanName,
+            boolean primary,
+            String[] dependencies
+    ) {
+        this.scope = scope;
+        this.beanClassName = beanClassName;
         this.beanName = beanName;
+        this.primary = primary;
         this.dependencies = DependencyConverter.convert(dependencies);
     }
 
