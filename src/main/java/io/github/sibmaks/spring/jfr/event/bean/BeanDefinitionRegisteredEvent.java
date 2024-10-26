df999e1de040dcf6d0f5fe5d24b4c813548a2796
sibmaks <sibmaks@vk.com>
2024-10-26 13:51:39 +0300

    Add generated flag

--- src/main/java/io/github/sibmaks/spring/jfr/event/bean/BeanDefinitionRegisteredEvent.java
+++ src/main/java/io/github/sibmaks/spring/jfr/event/bean/BeanDefinitionRegisteredEvent.java
@@ -21,23 +21,27 @@ public class BeanDefinitionRegisteredEvent extends Event {
     @Label("Bean name")
     private final String beanName;
     @Label("Bean primary flag")
-    private final boolean primary;
+    private final Boolean primary;
     @Label("Bean's dependencies")
     private final String dependencies;
+    @Label("Bean generated flag")
+    private final boolean generated;
 
     @Builder
     public BeanDefinitionRegisteredEvent(
             String scope,
             String beanClassName,
             String beanName,
-            boolean primary,
-            String[] dependencies
+            Boolean primary,
+            String[] dependencies,
+            boolean generated
     ) {
         this.scope = scope;
         this.beanClassName = beanClassName;
         this.beanName = beanName;
         this.primary = primary;
         this.dependencies = DependencyConverter.convert(dependencies);
+        this.generated = generated;
     }
 
     /**
