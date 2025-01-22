37df757ca2e95bee2aaef7930b3d5d9e9a878d66
sibmaks <sibmaks@vk.com>
2025-01-22 12:31:34 +0300

    Добавлено отдельное событие для публикации смердженого bean definition-а

---
+++ src/main/java/io/github/sibmaks/spring/jfr/event/api/bean/MergedBeanDefinitionRegisteredFact.java
@@ -0,0 +1,44 @@
+package io.github.sibmaks.spring.jfr.event.api.bean;
+
+import io.github.sibmaks.spring.jfr.event.core.converter.DependencyConverter;
+import jdk.jfr.Label;
+import jdk.jfr.Name;
+
+/**
+ * @author sibmaks
+ * @since 0.0.12
+ */
+public interface MergedBeanDefinitionRegisteredFact {
+    @Name("contextId")
+    @Label("Spring Context Id")
+    String getContextId();
+
+    @Name("scope")
+    @Label("Bean scope")
+    String getScope();
+
+    @Name("beanClassName")
+    @Label("Bean class name")
+    String getBeanClassName();
+
+    @Name("beanName")
+    @Label("Bean name")
+    String getBeanName();
+
+    @Name("primary")
+    @Label("Bean primary flag")
+    String getPrimary();
+
+    @Name("dependencies")
+    @Label("Bean's dependencies")
+    String getDependencies();
+
+    @Name("generated")
+    @Label("Bean generated flag")
+    boolean isGenerated();
+
+    default String[] getDependenciesAsArray() {
+        var dependencies = getDependencies();
+        return DependencyConverter.convert(dependencies);
+    }
+}
