6400e0a052cd51af2ffad49fa67b3a2808b054bf
sibmaks <sibmaks@vk.com>
2025-04-14 11:47:22 +0300

    Добавил события запросов в соединении
    Signed-off-by: sibmaks <sibmaks@vk.com>

--- common/src/main/java/io/github/sibmaks/spring/jfr/event/api/pool/jdbc/connection/action/ConnectionAction.java
+++ common/src/main/java/io/github/sibmaks/spring/jfr/event/api/pool/jdbc/connection/action/ConnectionAction.java
@@ -8,5 +8,12 @@ public enum ConnectionAction {
     CREATE,
     CLOSE,
     COMMIT,
-    ROLLBACK
+    ROLLBACK,
+
+    EXECUTE,
+    EXECUTE_QUERY,
+    EXECUTE_UPDATE,
+    EXECUTE_BATCH,
+    EXECUTE_LARGE_UPDATE,
+    EXECUTE_LARGE_BATCH,
 }
